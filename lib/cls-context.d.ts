b376dd733e65122abccdbd0b5550f3330b5d3a9a
Jefferson Cardoso <jeffersoncardoso.s@gmail.com>
2022-10-24 10:47:24 -0300

    first commit

---
+++ lib/cls-context.d.ts
@@ -0,0 +1,12 @@
+import { ClsContextNamespace } from "./cls-context-namespaces";
+declare class LocalStorageContext {
+    private storage;
+    run(next: Function): Promise<any>;
+    scope(): void;
+    setContext(namespace: ClsContextNamespace, context: object): void;
+    getContext(namespace: ClsContextNamespace): object;
+    setContextValue(key: string, value: unknown, namespace: ClsContextNamespace): void;
+    getContextValue(key: string, namespace: ClsContextNamespace): any;
+}
+declare const StorageContext: LocalStorageContext;
+export { StorageContext };
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+/* eslint-disable import/export */
+export * from './cls-context-namespaces';
+export * from './cls-context';
